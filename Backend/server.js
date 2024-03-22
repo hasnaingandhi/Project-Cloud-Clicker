982d09a8af89cc02890356ee6eb31caaa2cedf5f
hasnaingandhi <hasnain2804@gmail.com>
2024-03-22 15:00:47 -0400

    test

--- Backend/server.js
+++ Backend/server.js
@@ -8,11 +8,17 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'pwd@1234',
   database: 'cloudclickerdb'
+});*/
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 app.get('/counter', (req, res) => {
